4a59ca934cc13f0ef0273a004373865a91ddbf43
Diana <navbidibi@gmail.com>
2025-01-10 20:03:16 +0200

    add strict mode

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,11 +1,13 @@
+'use strict';
+
 // Refactor following solution
 // Extract substring between prefix and suffix
 
-getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p);
+const getvaluebetween = (str, p, s) => {
+  let i = str.indexOf(p);
   if (i === -1) return '';
   else {
-    k = i + p.length;
+    const k = i + p.length;
     str = str.substring(k);
     if (s) {
       i = str.indexOf(s);
