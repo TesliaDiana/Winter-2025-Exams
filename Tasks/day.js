0fc70c1c361449368e985d66b6a0538c95d1fe14
Diana <navbidibi@gmail.com>
2025-01-11 17:05:54 +0200

    use strict mode

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,9 +1,11 @@
+'use strict';
+
 // Refactor following solution
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
+const _parse_day_ = (s) => {
   let i;
   for (i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
