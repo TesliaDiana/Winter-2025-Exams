f57da321bbc35d3442e958bc230d5f76bb9a857d
Diana <navbidibi@gmail.com>
2025-01-11 17:03:45 +0200

    simplified

--- Tasks/count.js
+++ Tasks/count.js
@@ -3,12 +3,8 @@
 // Refactor following solution
 // Sum all number values in dict
 
-const count = (object) => {
-  let summa = 0;
-  for (const value of Object.values(object)) {
-    if (typeof value === 'number') summa += value;
-  }
-  return summa;
-};
+const count = (object) => Object.values(object).reduce(
+  (summa, value) => (typeof value === 'number' ? (summa += value) : summa), 0
+);
 
 module.exports = count;
