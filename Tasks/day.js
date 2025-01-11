b562da63f28bec3d3a88d3efa873979257795609
Diana <navbidibi@gmail.com>
2025-01-11 17:09:00 +0200

    renamed variables

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,13 +3,13 @@
 // Refactor following solution
 // Get day number
 
-const D = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
-const _parse_day_ = (s) => {
-  for (let i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i])) return i + 1;
+const parseDay = (day) => {
+  for (let i = 0; i < DAYS.length; i++) {
+    if (day.startsWith(DAYS[i])) return i + 1;
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
