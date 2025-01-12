51c280a0de0d1fdd632bd328118155a6d241c338
Diana <navbidibi@gmail.com>
2025-01-12 17:57:58 +0200

    renamed variables

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -3,10 +3,10 @@
 // Refactor following solution
 // Reverse an array, you can't use .reverse()
 
-const invert = (A) => {
-  const T = Object.keys(A);
-  T.forEach((_, i) => T[i] = A.pop());
-  return T;
+const invert = (array) => {
+  const indexes = Object.keys(array);
+  indexes.forEach((_, index) => indexes[index] = array.pop());
+  return indexes;
 };
 
 module.exports = invert;
