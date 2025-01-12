0609b45d4a60adc1cc8713608cfc877496dbb025
Diana <navbidibi@gmail.com>
2025-01-12 13:31:47 +0200

    simplified code

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -3,16 +3,8 @@
 // Refactor following solution
 // Filter array by type name
 
-const filter = (data, filteringType) => {
-  const removeIndexes = [];
-  for (const element of data) {
-    const index = data.indexOf(element);
-    if (typeof data[index] !== filteringType) {
-      removeIndexes.unshift(index);
-    }
-  }
-  for (const index of removeIndexes) data.splice(index, 1);
-  return data;
-};
+const filter = (data, filteringType) => data.filter(
+  (value) => typeof value === filteringType
+);
 
 module.exports = filter;
