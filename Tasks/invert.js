f0a5cc2e54e398cd5dabcd6d1f65160151f0bcd3
Diana <navbidibi@gmail.com>
2025-01-12 18:08:35 +0200

    simplified code

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -4,9 +4,13 @@
 // Reverse an array, you can't use .reverse()
 
 const invert = (array) => {
-  const indexes = Object.keys(array);
-  indexes.forEach((_, index) => indexes[index] = array.pop());
-  return indexes;
+  const reverseArray = [];
+
+  for (const value of array) {
+    reverseArray.unshift(value);
+  }
+
+  return reverseArray;
 };
 
 module.exports = invert;
