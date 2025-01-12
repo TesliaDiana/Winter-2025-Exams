08679b6ffd39cf1cf486f5c727736873ca2de0c5
Diana <navbidibi@gmail.com>
2025-01-12 13:33:37 +0200

    used strict mode

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,10 +1,12 @@
+'use strict';
+
 // Refactor following solution
 // Find key by value
 
-find = (object, ...rest) => {
-  value = rest.pop(1);
+const find = (object, ...rest) => {
+  const value = rest.pop(1);
   [];
-  for (name in object) {
+  for (const name in object) {
     if (object[name] !== value) {
     } else {
       if (typeof name) return name;
