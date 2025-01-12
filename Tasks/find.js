f2cf2b9a98b82c20f54ca9ef010dd00312349a4f
Diana <navbidibi@gmail.com>
2025-01-12 13:53:21 +0200

    simplified code

--- Tasks/find.js
+++ Tasks/find.js
@@ -3,14 +3,9 @@
 // Refactor following solution
 // Find key by value
 
-const find = (object, value) => {
-
-  for (const key in object) {
-
-    if (object[key] === value) return key;
-
-  }
-
-};
+const find = (object, searchingValue) => Object.entries(object).find(
+  // eslint-disable-next-line no-unused-vars
+  ([_, value]) => value === searchingValue
+)?.[0];
 
 module.exports = find;
