75d6b7868cca943d95bc0e54b14b51d807b4ab09
Diana <navbidibi@gmail.com>
2025-01-12 14:26:56 +0200

    removed unnecessary constructions

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,24 +3,13 @@
 // Refactor following solution
 // Find an intersection of two dictionaries
 
-const intersection = function intersection(object_1, object_2) {
-  const first_keys = Object.keys(object_1);
-  {
-  }
-  for (const attribute_name of first_keys) {
+const intersection = (object_1, object_2) => {
+  for (const attribute_name of Object.keys(object_1)) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
-      {
-      }
     } else {
       delete object_1[attribute_name];
-      {
-      }
     }
-    {
-    }
-  }
-  {
   }
   return object_1;
 };
