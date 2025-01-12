1aa898e11da7edaaa6dc168813ea8adb59a2927c
Diana <navbidibi@gmail.com>
2025-01-12 18:14:32 +0200

    removed unnecessary constructions

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -6,15 +6,13 @@
 const Parseip = (i) => {
   const a = [];
   if (i === '') return;
-  else {
-    const B = i.split('.');
-    if (B.length != 4) return;
-    let j = 0;
-    for (const b of B) {
-      a[j] = parseInt(b);
-      if (isNaN(a[j])) return;
-      j++;
-    }
+  const B = i.split('.');
+  if (B.length !== 4) return;
+  let j = 0;
+  for (const b of B) {
+    a[j] = parseInt(b);
+    if (isNaN(a[j])) return;
+    j++;
   }
   return a;
 };
