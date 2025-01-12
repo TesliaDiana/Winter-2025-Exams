44a109ae7ead737dac81567c0e5ec59f2fba28ed
Diana <navbidibi@gmail.com>
2025-01-12 18:17:15 +0200

    renamed variables

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -3,18 +3,18 @@
 // Refactor following solution
 // Split string by the first occurrence of separator
 
-const Parseip = (i) => {
-  const a = [];
-  if (i === '') return;
-  const B = i.split('.');
-  if (B.length !== 4) return;
-  let j = 0;
-  for (const b of B) {
-    a[j] = parseInt(b);
-    if (isNaN(a[j])) return;
-    j++;
+const parseip = (ip) => {
+  const numbersIp = [];
+  if (ip === '') return;
+  const splitIp = ip.split('.');
+  if (splitIp.length !== 4) return;
+  let index = 0;
+  for (const number of splitIp) {
+    numbersIp[index] = parseInt(number);
+    if (isNaN(numbersIp[index])) return;
+    index++;
   }
-  return a;
+  return numbersIp;
 };
 
-module.exports = Parseip;
+module.exports = parseip;
