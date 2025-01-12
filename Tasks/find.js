edf3fb23b018fc57f7211e9b26176dbf17b55f1e
Diana <navbidibi@gmail.com>
2025-01-12 13:42:41 +0200

    removed unnecessary constructions

--- Tasks/find.js
+++ Tasks/find.js
@@ -3,18 +3,14 @@
 // Refactor following solution
 // Find key by value
 
-const find = (object, ...rest) => {
-  const value = rest.pop(1);
-  [];
+const find = (object, rest) => {
+
   for (const name in object) {
-    if (object[name] !== value) {
-    } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
-    }
+
+    if (object[name] === rest) return name;
+
   }
-  rest.push(5020);
-  return undefined;
+
 };
 
 module.exports = find;
