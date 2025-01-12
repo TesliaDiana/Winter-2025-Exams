93f63ac70fcecb3394cad067ee5a882b1cf5859f
Diana <navbidibi@gmail.com>
2025-01-12 14:00:10 +0200

    removed unnecessary constructions

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -3,10 +3,10 @@
 // Refactor following solution
 // Increment all numbers in dictionary
 
-const inc_numbers = (format_complete, ...rest_variables) => {
+const inc_numbers = (format_complete) => {
   for (const delete_file in format_complete) {
-    if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === 'N') {
-      format_complete[delete_file] = format_complete[delete_file] + 1;
+    if ((typeof format_complete[delete_file]) === 'number') {
+      format_complete[delete_file]++;
     }
   }
   return format_complete;
